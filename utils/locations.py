fcfabfa9236a19c0f733884e41a2ef5c2b68b576
martijnbentum <bentummartijn@gmail.com>
2024-10-15 18:29:18 +0200

    module to link data

---
+++ utils/locations.py
@@ -0,0 +1,18 @@
+cgn_dir = '/vol/bigdata/corpora2/CGN2/'
+cgn_audio_dir = cgn_dir + 'data/audio/wav/'
+cgn_annot_dir = cgn_dir + 'data/annot/text/'
+cgn_phoneme_dir = cgn_annot_dir + 'fon/'
+cgn_ort_dir = cgn_annot_dir + 'ort/'
+cgn_awd_dir = cgn_annot_dir + 'awd/'
+cgn_fon_dir = cgn_annot_dir + 'fon/'
+cgn_speaker_file = cgn_dir + 'data/meta/text/speakers.txt'
+local_awd = '../awd/'
+local_fon= '../fon/'
+local_ort= '../ort/'
+
+
+cache_dir = '../WAV2VEC_DATA/'
+cgn_phrases = '../cgn_phrases/'
+wav_filenames_phrases_dict = '../cgn_wav_filename_phrases.json'
+
+

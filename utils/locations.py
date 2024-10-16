b0f69fdad474db3207bd3f4a2666213ed24eaec3
martijnbentum <bentummartijn@gmail.com>
2024-10-16 16:07:34 +0200

    add location for vocab and json

--- utils/locations.py
+++ utils/locations.py
@@ -1,3 +1,5 @@
+import json
+
 cgn_dir = '/vol/bigdata/corpora2/CGN2/'
 cgn_audio_dir = cgn_dir + 'data/audio/wav/'
 cgn_annot_dir = cgn_dir + 'data/annot/text/'
@@ -12,7 +14,11 @@ local_ort= '../ort/'
 
 
 cache_dir = '../WAV2VEC_DATA/'
+json_dir = '../JSONS/'
+vocab_file = cache_dir + 'vocab.json'
 cgn_phrases = '../cgn_phrases/'
-wav_filenames_phrases_dict = '../cgn_wav_filename_phrases.json'
+cgn_phrases_dict = '../cgn_wav_filename_phrases.json'
+cgn_speaker_dict = '../cgn_speakers.json'
 
-
+with open(vocab_file) as fin:
+    vocab = json.load(fin)

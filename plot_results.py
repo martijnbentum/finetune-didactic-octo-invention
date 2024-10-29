bf700a80f8a1e055056a10612f29888462a86358
martijnbentum <bentummartijn@gmail.com>
2024-10-29 15:46:23 +0100

    plot eval wer for finetuning

---
+++ plot_results.py
@@ -0,0 +1,32 @@
+import json
+from matplotlib import pyplot as plt
+
+def load_result_dict():
+    d = json.load(open('wer_results.json'))
+    return d
+
+def plot(wers, color = None, label = None):
+    plt.ion()
+    x = [line[1] for line in wers]
+    y = [line[0] for line in wers]
+    plt.plot(x,y, color = color, label = label)
+    return x, y
+
+def plot_orthographic(d = None, condition= 'orthographic'):
+    if not d: d = load_result_dict()
+    colors = ['red','blue','green','purple']
+    i = 0
+    plt.ion()
+    plt.figure()
+    for label, value in d.items():
+        wers = value[condition]
+        color = colors[i]
+        i += 1
+        label = str(label.strip('/'))
+        print([label,color])
+        plot(wers,color,label)
+    plt.legend()
+    plt.grid(alpha = .5)
+    plt.ylabel('wer')
+    plt.xlabel('training steps')
+    plt.title(condition)

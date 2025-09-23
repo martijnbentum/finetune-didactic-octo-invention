690f45a8842f9d7798ef4e82ce8ca9c841ee68e5
martijnbentum <bentummartijn@gmail.com>
2025-09-23 17:54:41 +0200

    updated location of pretrained models

--- utils/finetuner.py
+++ utils/finetuner.py
@@ -5,51 +5,21 @@ from pathlib import Path
 from utils import wav2vec2_model
 from utils import locations
 
-def check_pretrained_checkpoint_exists(checkpoint_name,directory = None ):
-    if not directory:
-        directory = '../w2v_pretrain_checkpoints/dutch_960/'
-    p = Path(directory)
-    filenames = list(p.glob(f'*_{checkpoint_name}.pt'))
-    if len(filenames) == 0:
-        return False
-    if len(filenames) > 1:
-        print(filenames)
-        raise ValueError('Multiple checkpoints found')
-    print(f'Checkpoint {checkpoint_name} found: {filenames[0]}')
-    return True
-
-def download_dutch_pretrained_checkpoint(name, output_directory = None):
-    if not output_directory:
-        output_directory = '../w2v_pretrain_checkpoints/dutch_960/'
-    cmd = config('scp_snel')
-    cmd += config('dutch_pretrain_dir')
-    if check_pretrained_checkpoint_exists(name):
-        print(f'Checkpoint {name} already exists, doing nothing.')
-        return
-    if name == 'best':
-        checkpoint_name = 'checkpoint_best.pt'
-    else:
-        checkpoint_name = f'checkpoint_*_{name}.pt'
-    cmd += checkpoint_name + ' ' + output_directory 
-    print(f'Downloading checkpoint... \n{cmd}')
-    os.system(cmd)
-    return cmd
-
-def download_dutch_pretrained_checkpoints():
-    checkpoints = [1,2,10,100,1000,10_000,100_000]
-    for checkpoint in checkpoints:
-        _ = download_dutch_pretrained_checkpoint(checkpoint)
-
-
+steps = [200_000, 10, 100_000, 1000, 10_000, 100, 50_000, 5000, 150_000]
 
 def collect_pretrained_checkpoints(pretrained_model_dir = None):
     if not pretrained_model_dir:
-        pretrained_model_dir = '../w2v_pretrain_checkpoints/dutch_960/'
+        pretrained_model_dir = locations.dutch_wav2vec2_the_first
     p = Path(pretrained_model_dir)
     dirs = p.glob('*/pytorch_model.bin')
-    return [str(d.parent) for d in dirs]
-
-
+    dirs = [str(d.parent) for d in dirs]
+    best_last = [x for x in dirs if 'best' in x or 'last' in x]
+    numbered = [x for x in dirs if 'best' not in x and 'last' not in x]
+    numbered = sorted(numbered, key = lambda x: int(x.split('_')[-1]))
+    if best_last:
+        all_checkpoints = best_last + numbered
+    else: all_checkpoints = numbered
+    return all_checkpoints
 
 def finetune_pretrained_checkpoint(checkpoint_dir, experiment_name ,
     dataset_name = 'o', transcription = 'orthographic'):
@@ -58,12 +28,33 @@ def finetune_pretrained_checkpoint(checkpoint_dir, experiment_name ,
     trainer = wav2vec2_model.load_trainer(dataset_name, transcription, 
         experiment_name)
     trainer.train()
+    return trainer
+
+def make_checkpoint_finetune_list(pretrained_model_dir, steps = steps):
+    cps = collect_pretrained_checkpoints(pretrained_model_dir)
+    output = []
+    for step in steps:
+        for cp in cps:
+            number = cp.split('_')[-1]
+            if not number.isdigit(): continue
+            number = int(number)
+            if number != step: continue
+            experiment_name = f'{cp.split("/")[-2]}_pt-{step}_ft-o/'
+            output.append({'checkpoint_dir':cp, 
+                'experiment_name':experiment_name})
+    return output
+
+def finetune_all_pretrained_checkpoints_in_list(pretrained_model_dir, steps):
+    finetune_list = make_checkpoint_finetune_list(pretrained_model_dir, steps)
+    for item in finetune_list:
+        print('working on ', item)
+        finetune_pretrained_checkpoint(**item)
+
+
      
     
     
 
-def handle_pretrained_model(name, checkpoints):
-    pass
 
 def load_large_model(directory = '' ):
     if not directory:

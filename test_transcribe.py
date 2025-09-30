554ddaea09e622c80467cd2b7aa07ada04487b3b
martijnbentum <bentummartijn@gmail.com>
2025-09-30 16:42:11 +0200

    updated load mls sentences; updated load test set

--- test_transcribe.py
+++ test_transcribe.py
@@ -6,11 +6,16 @@ from progressbar import progressbar
 from pathlib import Path
 
 
-def load_test_set(component = 'o', transcription = 'sampa'):
+def load_test_set(component = 'o', transcription = 'orthographic'):
     filename = locations.json_dir 
     filename += f'{component}_test_{transcription}.json'
     with open(filename) as fin:
         d = json.load(fin)
+    for line in d['data']:
+        if 'filename' in line:
+            line['audiofilename'] = line['filename']
+
+            line['sentence'] = line['text']
     return d
 
 def load_pipeline(recognizer_dir = None, device = -1, copy_helper_files = False):
@@ -37,7 +42,9 @@ def handle_test_set(recognizer_dir, component = 'o',
     pipeline = load_pipeline(recognizer_dir, device = device, 
         copy_helper_files = copy_helper_files)
     for line in progressbar(d['data']):
-        audio_filename = line['audiofilename']
+        if 'filename' in line: audio_filename = line['filename']
+        elif 'audiofilename' in line:audio_filename = line['audiofilename']
+        else: raise ValueError('No filename or audiofilename in line', line)
         line['hyp'] = apply_pipeline(pipeline, audio_filename)
     if save:
         with open(filename,'w') as fout:
@@ -66,7 +73,7 @@ def handle_ifadv(check_point, save = True, overwrite = False, device = -1):
     pipeline = load_pipeline(check_point, device = device)
     d = json.load(open('../JSON/ifadv_phrases.json'))
     filename = Path(check_point)
-    filename = filename / f'ifadv_test_hyp.json'
+    filename = filename / f'ifadv_test_orthographic_hyp.json'
     if filename.exists() and save and not overwrite:
         print('File exists, doing nothing', filename)
         return
@@ -79,20 +86,33 @@ def handle_ifadv(check_point, save = True, overwrite = False, device = -1):
     del pipeline
     return d
 
-def load_mls_sentences(split = 'test'):
+def _check_split(split):
+    if type(split) == str:
+        if split not in ['train','dev','test','all']:
+            raise ValueError('split should be train, dev, test or all')
+    if type(split) == list:
+        for s in split:
+            if s not in ['train','dev','test']:
+                raise ValueError('split should be train, dev or test')
+
+def load_mls_sentences(split = 'test', exclude_pretraining = True):
     with open('/vol/mlusers/mbentum/mls/dutch_mls_sentences_zs.tsv') as fin:
         lines = fin.read().split('\n')
     header, temp= lines[0], lines[1:]
     data = []
+    directory = Path('/vol/mlusers/mbentum/mls/dutch/audio/')
     for line in temp:
         d = {k:v for k,v in zip(header.split('\t'), line.split('\t'))}
         for h in ['start_time','end_time', 'duration']:
             d[h] = float(d[h])
         d['sentence'] = d['text'].strip('"')
-        d['in_pretraining'] = d['in_pretraining'] == 'True'
+        d['audiofilename'] = directory / d['audio_filename']
+        _check_split(split)
         if split == 'all': pass
-        elif split == 'in_pretraining' and d['in_pretraining']: continue
-        elif split in ['train','test','dev']:
+        elif exclude_pretraining and d['in_pretraining']: continue
+        elif type(split) == list:
+            if d['split'] not in split: continue
+        elif type(split) == str: 
             if d['split'] != split: continue
         data.append(d)
     return data
@@ -102,13 +122,12 @@ def handle_mls(check_point, save = True, overwrite = False, device = -1,
     pipeline = load_pipeline(check_point, device = device)
     d = load_mls_sentences(split)
     filename = Path(check_point)
-    filename = filename / f'mls_test_hyp.json'
+    filename = filename / f'mls_test_orthographic_hyp.json'
     if filename.exists() and save and not overwrite:
         print('File exists, doing nothing', filename)
         return
-    directory = Path('/vol/mlusers/mbentum/mls/dutch/audio/')
     for line in progressbar(d):
-        f = line['audio_filename']
+        f = line['audiofilename']
         audio_filename = directory / line['split'] / f
         line['hyp'] = apply_pipeline(pipeline, str(audio_filename))
     if save:
@@ -139,7 +158,7 @@ def handle_cv(check_point, save = True, overwrite = False, device = -1,
     pipeline = load_pipeline(check_point, device = device)
     d = load_cv_sentences(split)
     filename = Path(check_point)
-    filename = filename / f'cv_test_hyp.json'
+    filename = filename / f'cv_test_orthographic_hyp.json'
     if filename.exists() and save and not overwrite:
         print('File exists, doing nothing', filename)
         return
@@ -179,7 +198,7 @@ def handle_nbest(check_point, save = True, overwrite = False, device = -1):
     pipeline = load_pipeline(check_point, device = device)
     d = load_nbest_sentences()
     filename = Path(check_point)
-    filename = filename / f'nbest_test_hyp.json'
+    filename = filename / f'nbest_test_orthographic_hyp.json'
     if filename.exists() and save and not overwrite:
         print('File exists, doing nothing', filename)
         return
